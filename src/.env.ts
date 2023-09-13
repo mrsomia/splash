7e5c6c14b6e561a03f96d366add2012fd0d7d448
Sachin Somia <sachinsomia@gmail.com>
2023-09-13 21:00:33 +0100

    feat: env validation

---
+++ src/.env.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+const myEnv = z.object({
+  DATABASE_URL: z.string(),
+  SHADOW_DATABASE_URL: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+});
+
+myEnv.parse(process.env);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof myEnv> {}
+  }
+}
