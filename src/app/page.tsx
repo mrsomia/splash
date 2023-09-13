b7cd9bf650f82b306a644accd1401360c1d69df0
Sachin Somia <sachinsomia@gmail.com>
2023-09-13 21:00:50 +0100

    chore: updates content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center gap-12 p-24">
       <h1 className="text-xl">Splash tournament</h1>
       <div>
-        <div>An app to organise your tournament with</div>
+        <div>The tool for your bar tournament</div>
       </div>
     </main>
   );
