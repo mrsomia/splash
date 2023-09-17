fdb6c2b1ec62a595fcf69ead88d882b7712f5f63
Sachin Somia <sachinsomia@gmail.com>
2023-09-17 20:53:48 +0100

    refactor(schmea): Adds schedule and schedule to match tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,6 +6,7 @@ import {
   integer,
   varchar,
   boolean,
+  unique,
 } from "drizzle-orm/pg-core";
 import type { AdapterAccount } from "@auth/core/adapters";
 import { nanoid } from "nanoid";
@@ -72,7 +73,43 @@ export const tournaments = pgTable("tournament", {
   winner: text("winner"),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   startTime: timestamp("start_time").notNull().defaultNow(),
+  completedAt: timestamp("completed_at"),
 });
+
+export const schedule = pgTable("schedule", {
+  id: text("id")
+    .primaryKey()
+    .notNull()
+    .$defaultFn(() => nanoid()),
+  tournamentId: text("tournament_id")
+    .notNull()
+    .references(() => tournaments.id, {
+      onDelete: "cascade",
+    }),
+  currentRound: integer("current_round"),
+  totalRounds: integer("total_rounds"),
+});
+
+export const scheduleToMatches = pgTable(
+  "schedule_to_matches",
+  {
+    scheduleId: text("schedule_id")
+      .notNull()
+      .references(() => schedule.id),
+    matchId: text("match_id")
+      .notNull()
+      .references(() => matches.id),
+    round: integer("round").notNull(),
+    teamAId: text("team_a_id").references(() => teams.id),
+    teamBId: text("team_b_id").references(() => teams.id),
+    parentMatchId: text("parent_match_id").references(() => matches.id),
+  },
+  (stm) => ({
+    pk: primaryKey(stm.scheduleId, stm.matchId),
+    unq1: unique().on(stm.round, stm.scheduleId, stm.teamAId),
+    unq2: unique().on(stm.round, stm.scheduleId, stm.teamBId),
+  }),
+);
 
 export const teams = pgTable("teams", {
   id: text("id")
@@ -80,28 +117,45 @@ export const teams = pgTable("teams", {
     .notNull()
     .$defaultFn(() => nanoid()),
   name: text("name").notNull(),
-  tournamentId: text("tournament_id").references(() => tournaments.id, {
-    onDelete: "cascade",
-  }),
+  tournamentId: text("tournament_id")
+    .notNull()
+    .references(() => tournaments.id, {
+      onDelete: "cascade",
+    }),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   eliminated: boolean("eliminated").notNull().default(false),
 });
+
+export const userToTeams = pgTable(
+  "users_to_teams",
+  {
+    teamId: text("team_id")
+      .notNull()
+      .references(() => teams.id),
+    userId: text("user_id")
+      .notNull()
+      .references(() => users.id),
+    tournamentId: text("tournament_id")
+      .notNull()
+      .references(() => tournaments.id),
+  },
+  (utt) => ({
+    pk: primaryKey(utt.teamId, utt.userId, utt.tournamentId),
+  }),
+);
 
 export const matches = pgTable("match", {
   id: text("id")
     .primaryKey()
     .notNull()
     .$defaultFn(() => nanoid()),
-  teamA: text("team_a"),
-  teamB: text("team_b"),
   teamAScore: integer("team_a_score").default(0),
   teamBScore: integer("team_b_score").default(0),
-  tournamentId: text("tournament_id").references(() => tournaments.id, {
-    onDelete: "cascade",
-  }),
-  // NOTE: may need a previous game references
-  // previousMatchAId: text("previous_game_2_id"),
-  // previousMatchBId: text("previous_game_2_id"),
+  tournamentId: text("tournament_id")
+    .notNull()
+    .references(() => tournaments.id, {
+      onDelete: "cascade",
+    }),
   winner: varchar("winner", { enum: ["team1", "team2"] }),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   completedAt: timestamp("completed_at"),
