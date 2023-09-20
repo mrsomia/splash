1b1cbbd7c6b669372f664a8f7153e08663db63cd
Sachin Somia <sachinsomia@gmail.com>
2023-09-20 20:11:12 +0100

    feat: create tournament action

--- src/actions/tournament.ts
+++ src/actions/tournament.ts
@@ -1,7 +1,30 @@
 "use server";
 
-export async function createTournament(name: string, epochStart: number) {
-  const start = new Date(epochStart);
-  console.log({ name, start });
+import { createTournamentFromEmail } from "@/db/tournament";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
+export async function createTournament(
+  tournamentName: string,
+  epochStart: number,
+) {
+  const startTime = new Date(epochStart);
+  console.log({ tournamentName, startTime });
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
+  if (!email) {
+    console.info(
+      "Unable to find email for user session when createing new tournament",
+      { session },
+    );
+    throw new Error("Unable to find email for user session");
+  }
   //TODO: validate arguments
+
+  const tournament = await createTournamentFromEmail({
+    userEmail: email,
+    startTime,
+    tournamentName,
+  });
+  return tournament;
 }
