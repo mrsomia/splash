744d87e084cc9e51a39f820bf44e2b1feeb90a76
Sachin Somia <sachinsomia@gmail.com>
2023-09-21 20:56:44 +0100

    refactor(tournament-page):Adds signup link

--- src/app/tournament/[id]/page.tsx
+++ src/app/tournament/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getTournamentFromId } from "@/db/tournament";
 import { Suspense } from "react";
 import Spinner from "@/components/Spinner";
+import Link from "next/link";
 
 type PageProps = {
   params: {
@@ -11,13 +12,14 @@ type PageProps = {
 export default async function TournamentPage({ params }: PageProps) {
   console.log(params);
   const tournament = await getTournamentFromId(params.id);
+  console.log(tournament);
+  const start = new Date(tournament.startTime);
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-8">
+      <Link href={`/tournament/${tournament.id}/signup`}>signup</Link>
       <Suspense fallback={<Spinner />}>
         <h1>{tournament.name}</h1>
-        <span>{`starts at ${new Date(
-          tournament.startTime,
-        ).toLocaleTimeString()}`}</span>
+        <span>{`starts at ${start.toLocaleTimeString()} on ${start.toLocaleDateString()}`}</span>
       </Suspense>
     </main>
   );
