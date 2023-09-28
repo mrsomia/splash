0eb37821570916ec16502cf756dc4dd6f30d9dc8
Sachin Somia <sachinsomia@gmail.com>
2023-09-28 23:32:52 +0100

    refactor: logs error when unable to get tournament

--- src/app/tournament/[id]/signup/page.tsx
+++ src/app/tournament/[id]/signup/page.tsx
@@ -8,11 +8,15 @@ type PageProps = {
 };
 
 export default async function SignUpPage({ params }: PageProps) {
-  const teams = await getTeamsForTournament(params.id);
+  const teams = await getTeamsForTournament(params.id).catch((err) =>
+    console.error(err),
+  );
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-8">
       <h1>Signup Page</h1>
-      <p>{`${teams.length} team${teams.length !== 1 ? "s" : ""}`}</p>
+      {teams ? (
+        <p>{`${teams.length} team${teams.length !== 1 ? "s" : ""}`}</p>
+      ) : null}
       <NewTeamForm tournamentId={params.id} />
     </main>
   );
