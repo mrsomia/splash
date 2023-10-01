5a1e56801abcaec3756ce0035ac20d28b46969ad
Sachin Somia <sachinsomia@gmail.com>
2023-10-01 22:36:26 +0100

    refactor(schedule-game): Links to match page

--- src/app/tournament/[id]/schedule/Game.tsx
+++ src/app/tournament/[id]/schedule/Game.tsx
@@ -1,4 +1,5 @@
 import { getMatchesForTournament } from "@/db/tournament";
+import Link from "next/link";
 
 type GameProps = {
   match: Awaited<ReturnType<typeof getMatchesForTournament>>[number];
@@ -6,14 +7,16 @@ type GameProps = {
 
 export default function Game({ match }: GameProps) {
   return (
-    <div className="py-4">
-      <h5>{`Game ${match.matchNumber}`}</h5>
-      <div className="flex gap-4">
-        <span>{`${match.teamA ?? "TBD"} vs ${match.teamB ?? "TBD"}`}</span>
+    <Link href={`/tournament/${match.tournamentId}/match/${match.id}`}>
+      <div className="py-4">
+        <h5>{`Game ${match.matchNumber}`}</h5>
+        <div className="flex gap-4">
+          <span>{`${match.teamA ?? "TBD"} vs ${match.teamB ?? "TBD"}`}</span>
+        </div>
+        {match.winner ? (
+          <p>{`${match.winner == "teamA" ? match.teamA : match.teamB} won`}</p>
+        ) : null}
       </div>
-      {match.winner ? (
-        <p>{`${match.winner == "teamA" ? match.teamA : match.teamB} won`}</p>
-      ) : null}
-    </div>
+    </Link>
   );
 }
