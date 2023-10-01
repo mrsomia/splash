e8eb788f113a100defe38cc15d3738c8992ca022
Sachin Somia <sachinsomia@gmail.com>
2023-10-01 17:11:13 +0100

    feat: initial schedule page

--- src/app/tournament/[id]/schedule/page.tsx
+++ src/app/tournament/[id]/schedule/page.tsx
@@ -1,4 +1,9 @@
-import { getTournamentFromId } from "@/db/tournament";
+import {
+  getMatchesForTournament,
+  getMatchesForTournamentByRounds,
+  getTournamentFromId,
+} from "@/db/tournament";
+import Game from "./Game";
 
 type PageProps = {
   params: {
@@ -8,9 +13,22 @@ type PageProps = {
 
 export default async function TournamentPage({ params }: PageProps) {
   const tournament = await getTournamentFromId(params.id);
+  const rounds = await getMatchesForTournamentByRounds(params.id);
+  console.log(rounds);
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-8">
-      <h1>{`Schedule for ${tournament.name}`}</h1>
+      <h1 className="p-4">{`Schedule for ${tournament.name}`}</h1>
+      <div className={`flex items-center gap-4 px-4`}>
+        {rounds.reverse().map((round, idx) => (
+          <div key={idx} className="flex flex-col justify-center">
+            <h3>{`Round ${round[0].round ?? "unknown"}`}</h3>
+            {round.map((game) => (
+              <Game key={game.id} match={game} />
+            ))}
+          </div>
+        ))}
+        {}
+      </div>
     </main>
   );
 }
