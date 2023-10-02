fd0b269b360e751e166de2fbf6e5dc15d3b7b9e6
Sachin Somia <sachinsomia@gmail.com>
2023-10-02 23:43:54 +0100

    styles(dashboard): add button and new layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { Suspense } from "react";
+import { Plus } from "lucide-react";
 
 export default async function Dasboard() {
   const session = await getServerSession(authOptions);
@@ -13,20 +14,23 @@ export default async function Dasboard() {
   }
   return (
     <main className="flex min-h-screen flex-col items-center gap-12 p-24">
-      <h1 className="text-xl">Dashboard</h1>
       <div className="">
-        <Link href="/tournament/new" className="py-4">
-          start a new tournament
-        </Link>
+        <div className="flex w-100 justify-end gap-8">
+          <h1 className="text-xl">Previous Tournaments</h1>
+          <Link href="/tournament/new">
+            <button className="rounded-md bg-orange-600 p-2">
+              <Plus />
+            </button>
+          </Link>
+        </div>
         <Suspense>
           {previousTournaments && previousTournaments.length ? (
             <>
-              <h2 className="py-4">Previous Tournaments</h2>
               {previousTournaments.map((t) => (
-                <div key={t.id} className="flex p-2">
+                <div key={t.id} className="flex py-8">
                   <Link href={`/tournament/${t.id}`}>
                     <div className="flex flex-col gap-2">
-                      <h4>{t.name}</h4>
+                      <p className="capitalize text-lg py-2">{t.name}</p>
                       <span>{`starts at ${t.startTime?.toLocaleTimeString()} on ${t.startTime?.toLocaleDateString()}`}</span>
                     </div>
                   </Link>
