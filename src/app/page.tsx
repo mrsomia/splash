df13507562e17f8c5d2fb9a13696f9523f76c62d
Sachin Somia <sachinsomia@gmail.com>
2023-10-08 21:40:05 +0100

    styles(hero page): restyled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,195 @@
-import SignInButton from "@/components/SignInButton";
+import SignInButton, { SignUpButton } from "@/components/SignInButton";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  // if (session) {
+  //   redirect(`/dashboard`);
+  // }
   return (
-    <main className="flex min-h-screen flex-col items-center gap-12 p-24">
-      <h1 className="text-xl">Splash tournament</h1>
-      <div>
-        <div>The tool for your bar tournament</div>
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 ">
+      <div className="container mx-auto px-4 md:px-6">
+        <div className="grid gap-6 items-center">
+          <div className="flex flex-col justify-center space-y-8 text-center">
+            <div className="space-y-2">
+              <h1 className="py-1 text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
+                Splash Tournament Organiser
+              </h1>
+              <p className="max-w-[600px] text-zinc-200 md:text-xl dark:text-zinc-100 mx-auto">
+                Oraganise your next bar tournament with ease
+              </p>
+            </div>
+            <div className="flex justify-center py-4 gap-6">
+              <SignUpButton />
+              <SignInButton />
+            </div>
+            <div className="w-full max-w-full space-y-4 mx-auto">
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+                <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                  <div className="p-2 rounded-full">
+                    <svg
+                      className=" text-white h-6 w-6 mb-2 opacity-75"
+                      fill="none"
+                      height="24"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <polyline points="22 12 16 12 14 15 10 15 8 12 2 12" />
+                      <path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z" />
+                    </svg>
+                  </div>
+                  <h2 className="text-xl font-bold text-white">
+                    Automatic Scheduling
+                  </h2>
+                  <p className="text-zinc-200 dark:text-zinc-100">
+                    A Randomised schedule, that works with uneven numbers. So
+                    you don&#39;t need to have 16 or 32 teams exactly.
+                  </p>
+                </div>
+                <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                  <div className="p-2 rounded-full">
+                    <svg
+                      className=" text-white h-6 w-6 mb-2 opacity-75"
+                      fill="none"
+                      height="24"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="m8 6 4-4 4 4" />
+                      <path d="M12 2v10.3a4 4 0 0 1-1.172 2.872L4 22" />
+                      <path d="m20 22-5-5" />
+                    </svg>
+                  </div>
+                  <h2 className="text-xl font-bold text-white">
+                    Seamless Integration
+                  </h2>
+                  <p className="text-zinc-200 dark:text-zinc-100">
+                    Allow all players and teams to track the tournament
+                    themselves.
+                  </p>
+                </div>
+                <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                  <div className="p-2 bg-opacity-50 rounded-full">
+                    <svg
+                      className=" text-white h-6 w-6 mb-2 opacity-75"
+                      fill="none"
+                      height="24"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+                      <circle cx="12" cy="12" r="3" />
+                    </svg>
+                  </div>
+                  <h2 className="text-xl font-bold text-white">
+                    Notifications
+                  </h2>
+                  <p className="text-zinc-200 dark:text-zinc-100">
+                    COMING SOON: Notifications to call teams over for their next
+                    game
+                  </p>
+                </div>
+                {/* <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"> */}
+                {/*   <div className="p-2 rounded-full"> */}
+                {/*     <svg */}
+                {/*       className=" text-white h-6 w-6 mb-2 opacity-75" */}
+                {/*       fill="none" */}
+                {/*       height="24" */}
+                {/*       stroke="currentColor" */}
+                {/*       strokeLinecap="round" */}
+                {/*       strokeLinejoin="round" */}
+                {/*       strokeWidth="2" */}
+                {/*       viewBox="0 0 24 24" */}
+                {/*       width="24" */}
+                {/*       xmlns="http://www.w3.org/2000/svg" */}
+                {/*     > */}
+                {/*       <circle cx="11" cy="11" r="8" /> */}
+                {/*       <path d="m21 21-4.3-4.3" /> */}
+                {/*     </svg> */}
+                {/*   </div> */}
+                {/*   <h2 className="text-xl font-bold text-white"> */}
+                {/*     Powerful Search */}
+                {/*   </h2> */}
+                {/*   <p className="text-zinc-200 dark:text-zinc-100"> */}
+                {/*     Our Powerful Search feature allows you to find any email, */}
+                {/*     contact, or file in seconds. */}
+                {/*   </p> */}
+                {/* </div> */}
+                {/* <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"> */}
+                {/*   <div className="p-2 rounded-full"> */}
+                {/*     <svg */}
+                {/*       className=" text-white h-6 w-6 mb-2 opacity-75" */}
+                {/*       fill="none" */}
+                {/*       height="24" */}
+                {/*       stroke="currentColor" */}
+                {/*       strokeLinecap="round" */}
+                {/*       strokeLinejoin="round" */}
+                {/*       strokeWidth="2" */}
+                {/*       viewBox="0 0 24 24" */}
+                {/*       width="24" */}
+                {/*       xmlns="http://www.w3.org/2000/svg" */}
+                {/*     > */}
+                {/*       <rect height="11" rx="2" ry="2" width="18" x="3" y="11" /> */}
+                {/*       <path d="M7 11V7a5 5 0 0 1 10 0v4" /> */}
+                {/*     </svg> */}
+                {/*   </div> */}
+                {/*   <h2 className="text-xl font-bold text-white"> */}
+                {/*     Reliable Security */}
+                {/*   </h2> */}
+                {/*   <p className="text-zinc-200 dark:text-zinc-100"> */}
+                {/*     With Reliable Security, your data is always safe and */}
+                {/*     protected. */}
+                {/*   </p> */}
+                {/* </div> */}
+                {/* <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"> */}
+                {/*   <div className="p-2 rounded-full"> */}
+                {/*     <svg */}
+                {/*       className=" text-white h-6 w-6 mb-2 opacity-75" */}
+                {/*       fill="none" */}
+                {/*       height="24" */}
+                {/*       stroke="currentColor" */}
+                {/*       strokeLinecap="round" */}
+                {/*       strokeLinejoin="round" */}
+                {/*       strokeWidth="2" */}
+                {/*       viewBox="0 0 24 24" */}
+                {/*       width="24" */}
+                {/*       xmlns="http://www.w3.org/2000/svg" */}
+                {/*     > */}
+                {/*       <path d="m8 6 4-4 4 4" /> */}
+                {/*       <path d="M12 2v10.3a4 4 0 0 1-1.172 2.872L4 22" /> */}
+                {/*       <path d="m20 22-5-5" /> */}
+                {/*     </svg> */}
+                {/*   </div> */}
+                {/*   <h2 className="text-xl font-bold text-white"> */}
+                {/*     Easy Collaboration */}
+                {/*   </h2> */}
+                {/*   <p className="text-zinc-200 dark:text-zinc-100"> */}
+                {/*     Easy Collaboration allows you to share and edit documents */}
+                {/*     with your team in real time. */}
+                {/*   </p> */}
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      {session ? (
-        <Link href="/dashboard">Go to dashboard</Link>
-      ) : (
-        <SignInButton />
-      )}
-    </main>
+    </section>
   );
 }
--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -2,5 +2,23 @@
 import { signIn } from "next-auth/react";
 
 export default function SignInButton() {
-  return <button onClick={() => signIn()}>Sign in</button>;
+  return (
+    <button
+      className="text-center px-4 lg:px-6 py-2 rounded-lg border-2 border-zinc-100 text-zinc-100 font-medium lg:font-semibold"
+      onClick={() => signIn()}
+    >
+      Sign In
+    </button>
+  );
+}
+
+export function SignUpButton() {
+  return (
+    <button
+      className="text-center px-4 lg:px-6 py-2 rounded-lg bg-zinc-200 text-black font-medium lg:font-semibold"
+      onClick={() => signIn()}
+    >
+      Sign Up
+    </button>
+  );
 }
