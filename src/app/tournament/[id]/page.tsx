cd6ca102e442d61bb9138e1a243152ac62ce4afb
Sachin Somia <sachinsomia@gmail.com>
2023-10-09 22:33:38 +0100

    refactor(tournament page): uses new header and nav

--- src/app/tournament/[id]/page.tsx
+++ src/app/tournament/[id]/page.tsx
@@ -7,9 +7,10 @@ import { getTeamsForTournament } from "@/db/teams";
 import { Suspense } from "react";
 import Spinner from "@/components/Spinner";
 import Link from "next/link";
-import ScheduleButton from "@/components/ScheduleButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import TournamentHeader from "@/components/TournamentHeader";
+import TournamentNav from "@/components/TournamentNav";
 
 type PageProps = {
   params: {
@@ -33,31 +34,44 @@ export default async function TournamentPage({ params }: PageProps) {
   console.log({ isAdmin });
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center gap-8">
+    <main className="min-h-screen mx-8 my-4 md:my-20 md:max-w-4xl md:mx-auto">
       <Suspense fallback={<Spinner />}>
-        <h1 className="text-xl capitalize">{tournament.name}</h1>
-        <div className="flex gap-4">
-          {isAdmin ? (
-            <Link href={`/tournament/${tournament.id}/signup`}>
-              <button className={`rounded-md bg-orange-600 p-2`}>
-                Add Teams
-              </button>
-            </Link>
-          ) : null}
-          {games.length ? (
-            <Link href={`/tournament/${tournament.id}/schedule`}>
-              <button className="rounded-md bg-orange-600 p-2">Schedule</button>
-            </Link>
-          ) : (
-            <ScheduleButton id={params.id} disabled={teams.length < 2} />
-          )}
+        <TournamentHeader
+          name={tournament.name}
+          totalTeams={teams.length}
+          start={start}
+        />
+        <TournamentNav
+          id={params.id}
+          isAdmin={isAdmin}
+          started={games.length > 0}
+        />
+        <div className="flex justify-between py-4">
+          {/* TODO: Make this interactive */}
+          <h2 className="text-2xl font-medium">Current Active Games</h2>
+          <ul className="flex gap-8 justify-center">
+            <li>12 vs 2</li>
+            <li>15 vs 18</li>
+            <li>16 vs 9</li>
+            <li>17 vs 28</li>
+          </ul>
         </div>
-        <p>{`on ${start.toLocaleDateString()}`}</p>
-        <p>{`starts at ${start.toLocaleTimeString()}`}</p>
         {teams.length ? (
-          <Link href={`/tournament/${params.id}/teams`}>
-            <p>{`${teams.length} team${teams.length !== 1 ? "s" : ""}`}</p>
-          </Link>
+          <>
+            <div className="flex py-4 items-end w-full">
+              <h2 className="text-2xl font-medium">Teams</h2>
+            </div>
+            <div className="py-2 grid grid-cols-2 md:grid-cols-3 m-auto">
+              {teams.map((team) => (
+                <Link
+                  href={`/tournament/${params.id}/teams/${team.id}`}
+                  key={team.id}
+                >
+                  <p className="px-2 py-1 text-center">{team.name}</p>
+                </Link>
+              ))}
+            </div>
+          </>
         ) : null}
       </Suspense>
     </main>
