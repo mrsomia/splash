3429e61bca51a0b4df202592774eae8ca91a87c2
Sachin Somia <sachinsomia@gmail.com>
2023-10-10 18:50:02 +0100

    styles(Tournament Header): Mobile Spacing

--- src/components/TournamentHeader.tsx
+++ src/components/TournamentHeader.tsx
@@ -9,14 +9,22 @@ export default function TournamentHeader({
   totalTeams,
 }: TournamentHeaderProps) {
   return (
-    <div className="flex items-center py-4">
-      <h1 className="text-3xl capitalize font-semibold py-6 mr-20">{name}</h1>
-      <span className="block mr-auto mb-4 self-end">{`${totalTeams} team${
+    <div className="flex items-center py-2 md:py-4">
+      <h1 className="text-xl md:text-3xl capitalize font-semibold py-3 md:py-6 mr-6 md:mr-16">
+        {name}
+      </h1>
+      <span className="block mr-auto">{`${totalTeams} team${
         totalTeams !== 1 ? "s" : ""
       }`}</span>
       <div>
-        <p>{`on ${start.toLocaleDateString()}`}</p>
-        <p>{`starts at ${start.toLocaleTimeString()}`}</p>
+        <p>
+          <span className="hidden sm:inline">on </span>
+          <span>{start.toLocaleDateString()}</span>
+        </p>
+        <p>
+          <span className="hidden sm:inline">starts at </span>
+          <span>{start.toLocaleTimeString()}</span>
+        </p>
       </div>
     </div>
   );
