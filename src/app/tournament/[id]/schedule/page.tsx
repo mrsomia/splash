66a66ad294da90e8f6a58574a7159dbdfb2797cb
Sachin Somia <sachinsomia@gmail.com>
2023-10-11 19:47:07 +0100

    refactor(schedule layout): Moves to a single file

--- src/app/tournament/[id]/schedule/page.tsx
+++ src/app/tournament/[id]/schedule/page.tsx
@@ -1,6 +1,7 @@
 import { getMatchesForTournamentByRounds } from "@/db/tournament";
-import Game from "@/components/Game";
-import { cn } from "@/lib/utils";
+import ScheduleLayout from "@/components/ScheduleLayout";
+import { Suspense } from "react";
+import Spinner from "@/components/Spinner";
 
 type PageProps = {
   params: {
@@ -12,22 +13,8 @@ export default async function SchedulePage({ params }: PageProps) {
   const rounds = await getMatchesForTournamentByRounds(params.id);
 
   return (
-    <div className={cn(`py-8 overflow-x-scroll snap-x min-w-full`)}>
-      <div className="flex mx-8 md:mx-auto">
-        {rounds.reverse().map((round, idx) => (
-          <div
-            key={idx}
-            className="snap-center text-center flex flex-col justify-center mx-6"
-          >
-            {/* <h2 className="hidden md:block">{round[0].round}</h2> */}
-            <div className="flex flex-col gap-2 justify-center">
-              {round.map((game) => (
-                <Game key={game.id} match={game} totalRounds={rounds.length} />
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Suspense fallback={<Spinner />}>
+      <ScheduleLayout rounds={rounds} />
+    </Suspense>
   );
 }
--- src/components/ScheduleLayout.tsx
+++ src/components/ScheduleLayout.tsx
@@ -1,13 +1,41 @@
-import { getMatchesForTournament } from "@/db/tournament";
+import {
+  getMatchesForTournament,
+  getMatchesForTournamentByRounds,
+} from "@/db/tournament";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+
+type ScheduleLayoutProps = {
+  rounds: Awaited<ReturnType<typeof getMatchesForTournamentByRounds>>;
+};
+export default function ScheduleLayout({ rounds }: ScheduleLayoutProps) {
+  return (
+    <div className={cn(`py-8 overflow-x-scroll snap-x min-w-full`)}>
+      <div className="flex px-8 md:mx-auto">
+        {rounds.reverse().map((round, idx) => (
+          <div
+            key={idx}
+            className="snap-center text-center flex flex-col justify-center px-10"
+          >
+            {/* <h2 className="hidden md:block">{round[0].round}</h2> */}
+            <div className="flex flex-col gap-2 justify-center">
+              {round.map((game) => (
+                <Game key={game.id} match={game} totalRounds={rounds.length} />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 type GameProps = {
   match: Awaited<ReturnType<typeof getMatchesForTournament>>[number];
   totalRounds: number;
 };
 
-export default function Game({ match, totalRounds }: GameProps) {
+export function Game({ match, totalRounds }: GameProps) {
   return (
     <Link href={`/tournament/${match.tournamentId}/match/${match.id}`}>
       <div className="w-60 bg-grey-200 flex items-center py-1 px-4">
