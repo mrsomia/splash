f4632ff5e730ccadbfd57ebc9bb2a286cf502a5b
Sachin Somia <sachinsomia@gmail.com>
2023-10-17 23:15:26 +0100

    feat(utils): errorToString function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function errorToString(e: unknown): string {
+  if (typeof e === "string") {
+    return e;
+  } else if (e instanceof Error) {
+    console.error(e);
+    return e.message;
+  } else if (e && typeof e === "object" && "error" in e) {
+    console.error(e.error);
+    if (typeof e.error === "string") {
+      return e.error;
+    }
+  } else {
+    console.error(e);
+  }
+  return "Oops something went wrong";
+}
