0d454deb3657d43e0f954c1df76873a72f0fbd7b
Sachin Somia <sachinsomia@gmail.com>
2023-10-22 17:45:47 +0100

    feat(subscribe button): Adds subscription to teams

---
+++ src/components/SubscribeButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { subscribeTotTeamNotificaitons } from "@/actions/teams";
+
+type SubscribeButtonProps = {
+  tournamentId: string;
+  teamId: string;
+};
+
+export default function SubscribeButton({
+  tournamentId,
+  teamId,
+}: SubscribeButtonProps) {
+  const handleClick = async () => {
+    const sw = await navigator.serviceWorker.ready;
+    const push = await sw.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: process.env.NEXT_PUBLIC_VAPID_ID_KEY,
+    });
+    console.log(JSON.stringify(push));
+    await subscribeTotTeamNotificaitons({
+      teamId,
+      tournamentId,
+      sub: JSON.stringify(push),
+    });
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className="my-4 py-2 px-4 bg-green-700 hover:bg-green-900 disabled:bg-slate-400 rounded-full w-3/4 self-center"
+    >
+      Get Notified
+    </button>
+  );
+}
