8b77436c03c8c97d55e4be41f38f7b8c0052d434
Sachin Somia <sachinsomia@gmail.com>
2023-10-23 00:33:29 +0100

    feat(start match): Notifies subscriptions

--- src/actions/match.ts
+++ src/actions/match.ts
@@ -6,6 +6,8 @@ import {
 } from "@/db/match";
 import { revalidatePath } from "next/cache";
 import { isUserATournamentAdmin } from "./tournament";
+import { deleteSubscription, getMatchNotificaitons } from "@/db/notifications";
+import { push } from "@/lib/notifications";
 
 export async function setMatchWinner({
   matchId,
@@ -58,5 +60,48 @@ export async function startMatch({
     console.error(`Error starting match: ${matchId}`);
     console.error(e);
     return "Error starting match";
+  }
+
+  // Rest of function handles notifications
+  let subs;
+  try {
+    subs = await getMatchNotificaitons(matchId);
+
+    console.log(subs);
+  } catch (e) {
+    console.error(
+      `Error getting match notifications subscriptions: ${matchId}`,
+    );
+    console.error(e);
+    return "Error notifying teams for match - could not get subscriptions";
+  }
+
+  try {
+    subs.forEach(async (subObj) => {
+      if (!subObj.sub) return;
+      const sub = JSON.parse(subObj.sub);
+      const title = `Team ${subObj.team} have a game starting`;
+      const message = `Game ${subObj.matchNumber} is starting, ${subObj.teamA} vs ${subObj.teamB}`;
+      const link = `http${process.env.NODE_ENV == "production" ? "s" : ""}://${
+        process.env.VERCEL_URL
+      }/tournament/${tournamentId}`;
+      const payload = { title, message, link };
+      console.log(
+        "sending push notification to: ",
+        sub,
+        JSON.stringify(payload),
+      );
+      await push.sendNotification(sub, JSON.stringify(payload)).catch((e) => {
+        if (e.statusCode === 404 || e.statusCode === 410) {
+          console.log("Subscription has expired or is no longer valid: ", e);
+          return deleteSubscription(sub.id);
+        } else {
+          throw e;
+        }
+      });
+    });
+  } catch (e) {
+    console.error(e);
+    return "Error sending notification to subscriptions";
   }
 }
