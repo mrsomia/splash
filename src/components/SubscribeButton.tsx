b2a803fad4ac5a6ee4d526f2d466e0cb225cdd59
Sachin Somia <sachinsomia@gmail.com>
2023-10-23 18:14:17 +0100

    WIP(Subscribe button): Checks notiication permission

--- src/components/SubscribeButton.tsx
+++ src/components/SubscribeButton.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { subscribeTotTeamNotificaitons } from "@/actions/teams";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 type SubscribeButtonProps = {
@@ -12,6 +14,41 @@ export default function SubscribeButton({
   tournamentId,
   teamId,
 }: SubscribeButtonProps) {
+  const [isPushNotificationSupported, setIsPushNotificationSupported] =
+    useState(true);
+  const [notificationPermission, setNotificationPermission] = useState<
+    typeof Notification.permission | null
+  >(null);
+  useEffect(() => {
+    if (window) {
+      setIsPushNotificationSupported(
+        "serviceWorker" in navigator && "PushManager" in window,
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (window) {
+      setNotificationPermission(Notification.permission);
+    }
+  }, []);
+
+  if (!isPushNotificationSupported) {
+    return (
+      <p className="text-center self-center py-2">
+        Push Notifications are not supported on this device currently
+      </p>
+    );
+  }
+
+  if (notificationPermission === "denied") {
+    return (
+      <p className="text-center self-center py-2">
+        You have disabled notifications
+      </p>
+    );
+  }
+
   const handleClick = async () => {
     const sw = await navigator.serviceWorker.ready;
     const push = await sw.pushManager
@@ -36,6 +73,7 @@ export default function SubscribeButton({
     }
     toast.success("Subscribed, you shoud be notified when your team play");
   };
+
   return (
     <button
       onClick={handleClick}
@@ -45,3 +83,34 @@ export default function SubscribeButton({
     </button>
   );
 }
+
+// export function RequestPermissionButton() {
+//   const router = useRouter();
+//   const getNotificationPersmission = async () => {
+//     if (
+//       Notification.permission === "granted" ||
+//       Notification.permission === "denied"
+//     ) {
+//       console.log("Notification permission: ", Notification.permission);
+//       router.refresh();
+//       return;
+//     }
+//     try {
+//       await Notification.requestPermission();
+//       router.refresh();
+//     } catch (e) {
+//       console.error(e);
+//       toast("Unable to have permisson granted");
+//     } finally {
+//       router.refresh();
+//     }
+//   };
+//   return (
+//     <button
+//       onClick={getNotificationPersmission}
+//       className="my-4 py-2 px-4 bg-green-700 hover:bg-green-900 disabled:bg-slate-400 rounded-full w-60 self-center"
+//     >
+//       Grant permission to notify
+//     </button>
+//   );
+// }
