7fb8c8f5bf4a1f8acae92757779918d6ecc415e9
Sachin Somia <sachinsomia@gmail.com>
2023-10-24 18:57:41 +0100

    feat(prod studio): Adds npm command to access prod DB

---
+++ prod-drizzle-config.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import type { Config } from "drizzle-kit";
+
+dotenv.config({ path: "./.env.production.local" });
+
+export default {
+  schema: "./src/db/schema.ts",
+  out: "./src/db/migrations",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
