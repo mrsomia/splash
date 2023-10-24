72e49e2ba5d0c66a9032a31d8a17a1f964c5402b
Sachin Somia <sachinsomia@gmail.com>
2023-10-24 19:20:00 +0100

    feat(Google login): Adds google authentication

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,8 @@ const myEnv = z.object({
   SHADOW_DATABASE_URL: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
   DISCORD_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
   NEXT_PUBLIC_VAPID_ID_KEY: z.string(),
   PRIVATE_VAPID_ID_KEY: z.string(),
 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db/index";
 
@@ -9,6 +10,10 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 };
